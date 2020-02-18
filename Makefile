c7a9b8c8c7ef43dd09409e6c51c8d4cae8e09879
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2020-02-18 13:26:23 +0300

    Publish task

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ format:
 
 get:
 	go run ./cmd/get
+
+publish:
+	make get && ./scripts/publish.sh
---
+++ scripts/publish.sh
@@ -0,0 +1,6 @@
+git reset -q HEAD -- .
+git add edmx/*.xml
+if [[ $(git diff --name-only --cached) != "" ]]; then
+  git commit -m "api metadata update, $(date +'%Y-%m-%d')"
+  put push
+fi
